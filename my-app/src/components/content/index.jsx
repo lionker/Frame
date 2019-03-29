2fd00796030ce8507d21d80262db9d06b5797352
Huang56 <635837244@qq.com>
2019-03-29 08:43:48 +0800

    'scaffold-init'

---
+++ my-app/src/components/content/index.jsx
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import './index.css'
+export default class Content extends Component {
+    render() {
+        return <h2 className='title'>this is content</h2>
+    }
+}
